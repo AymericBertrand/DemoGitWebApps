5b2e062947b3150e9f7c01c799f1b56757b5bd02
Bertrand Aymeric <aymbertrand@gmail.com>
2023-03-03 11:48:28 +0100

    SetInfo_01 : Personnal and Exp

---
+++ cv/info.js
@@ -0,0 +1,7 @@
+const mesInfos = {
+    nom : "Bertrand",
+    prenom : "Aymeric",
+    age : 34,
+    adresse : "Bld pire L desnouettes",
+    tel : "0492072272"
+};
---
+++ myExp.js
@@ -0,0 +1,7 @@
+const monXp = {
+    travail : "responsable de salle",
+    domaine : " horeca",
+    lieu : "waterloo",
+    dateDebut : "2009",
+    dateFin : "2022",
+};
